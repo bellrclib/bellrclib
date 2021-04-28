8035d3008b900ca8c5d7f854c2c716534df575c3
Bell RCLib <bellrclib@gmail.com>
2021-04-28 13:57:53 -0400

    Calc for discount and discount lookup fnc

--- src/lib/rclib.ts
+++ src/lib/rclib.ts
@@ -1,3 +1,4 @@
+/* eslint-disable functional/no-let */
 /* eslint-disable functional/immutable-data */
 /* eslint-disable functional/no-class */
 /* eslint-disable functional/prefer-readonly-type */
@@ -31,10 +32,42 @@
     return subTot;
   }
 
+  private calcDiscount(subTot: number): number {
+    const discount = this.lookupDiscountForVolume(subTot);
+    const discountAmount = subTot * (discount / 100);
+    return discountAmount;
+  }
+
   calcTotalPrice(): number {
     this.subTot = this.calcSubTot(this.qty, this.price)
+    const discountAmount = this.calcDiscount(this.subTot)
 
     return this.subTot // TODO: Fix interim calc
+  }
+
+  // Util
+  private lookupDiscountForVolume(orderValue: number): number {
+    let discount = 0;
+    switch (true) {
+      case orderValue < 1000:
+        discount = 0;
+        break;
+      case orderValue < 5000:
+        discount = 3;
+        break;
+      case orderValue < 7000:
+        discount = 5;
+        break;
+      case orderValue < 10000:
+        discount = 7;
+        break;
+      case orderValue >= 10000:
+        discount = 10;
+        break;
+      default:
+        break;
+    }
+    return discount;
   }
 
 }
