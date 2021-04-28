6793450fd9e7cadd75ed45d2957af6449e184f93
Bell RCLib <bellrclib@gmail.com>
2021-04-28 13:38:48 -0400

    Cleaned up cases to make params explicit

--- src/lib/rclib.spec.ts
+++ src/lib/rclib.spec.ts
@@ -6,18 +6,22 @@ import { ResellerCosts } from './rclib';
 //     Input:  500 items, $1 per item, Ontario
 //     Output: $565.00
 test('should calc total price for 500 items @ $1 in ON', (t) => {
-  let rc = new ResellerCosts(500,1,'ON');
+  const qty = 500;
+  const price = 1;
+  const provState = 'ON';
+  let rc = new ResellerCosts(qty, price, provState);
 
   t.is(rc.calcTotalPrice(), 565);
 });
-
 
 // ## Example 2:
 //     Input:  3600 items, $2.25 per item, Michigan
 //     Output: $7984.98
 test('should calc total price for 3600 items @ $2.25 in MI', (t) => {
-
-  let rc = new ResellerCosts(3600, 2.25, 'MI');
+  const qty = 3600;
+  const price = 2.25;
+  const provState = 'MI';
+  let rc = new ResellerCosts(qty, price, provState);
 
   t.is(rc.calcTotalPrice(), 7984.98);
 });
